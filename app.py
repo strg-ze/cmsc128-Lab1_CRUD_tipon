f19dda4f323f66e6ed0fb297ab4eb4f48b8da373
Adrienne Nicole Tipon <avtipon@up.edu.ph>
2026-09-07 23:41:08 +0800

    feat: updated app.py tasks route

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from init_db import get_db_connection
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,9 +8,27 @@ app = Flask(__name__)
 def dashboard():
     return render_template("dashboard.html")
 
-@app.route("/tasks")
+@app.route("/tasks", methods=['GET', 'POST'])
 def tasks():
-    return render_template("tasks.html")
+    conn = get_db_connection()
+    if request.method == 'POST':
+        conn.execute(
+            'INSERT INTO tasks (name, description, priority, due_date, time, tags, category) VALUES(?, ?, ?, ?, ?, ?, ?)',
+            (
+                request.form['task-name'],
+                request.form['description'],
+                request.form['priority'],
+                request.form['due-date'],
+                request.form['time'],
+                request.form['tags'],
+                request.form['category']            
+            )
+        )
+        conn.commit()
+
+    all_tasks = conn.execute('SELECT * FROM tasks WHERE is_visible = 1').fetchall()
+    conn.close()
+    return render_template("tasks.html", tasks = all_tasks)
 
 @app.route("/calendar")
 def calendar():
--- init_db.py
+++ init_db.py
@@ -1,10 +1,9 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
-
-connection.close()
 
 print("Database initialized.")
